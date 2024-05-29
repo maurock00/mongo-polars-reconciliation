75bcb7f580d1a1e06685f6749ad773b8c74e5c39
maurock00 <mauricio@kushkipagos.com>
2024-05-29 10:43:55 -0500

    [ARC-003] Now support iterations to use another keys or others

--- src/reconciliatior.py
+++ src/reconciliatior.py
@@ -7,6 +7,7 @@ class Reconciliator:
     def __init__(
         self, join_exp: Dict, odl_df: pl.DataFrame, file_df: pl.DataFrame
     ) -> None:
+        self.iterations = 1
         self.odl_df = odl_df
         self.file_df = file_df
         self.a_to_b_mt = pl.DataFrame()
@@ -36,7 +37,7 @@ class Reconciliator:
             #     [cs.starts_with("ext_"), cs.starts_with("exd_")]
             # ).rename(dup_rename_exp)
 
-            # To recreate original external dataframe imported with duplicated columns to allow duplicate processing
+            # To recreate original external dataframe imported repeating rc_keys columns to allow duplicate processing
             add_cols_exp = []
             print(nc_clean_dup_df.columns)
             print(self.join_exp["b_columns"])
@@ -48,7 +49,9 @@ class Reconciliator:
             no_conciliated_dup_df = nc_clean_dup_df.select(
                 [cs.starts_with("ext_"), cs.starts_with("exd_")]
             ).with_columns(add_cols_exp)
-            self.b_to_a_nmt = pl.concat([self.b_to_a_nmt, no_conciliated_dup_df])
+            self.b_to_a_nmt = pl.concat(
+                [self.b_to_a_nmt, no_conciliated_dup_df], how="diagonal"
+            )
 
             clean_df = df.unique(subset=[col_name], keep="first", maintain_order=True)
             return clean_df
@@ -56,9 +59,9 @@ class Reconciliator:
             return df
 
     def save_to_file(self):
-        self.a_to_b_mt.write_csv("./results/a_to_b_mt.csv")
-        self.a_to_b_nmt.write_csv("./results/a_to_b_nmt.csv")
-        self.b_to_a_nmt.write_csv("./results/b_to_a_nmt.csv")
+        self.a_to_b_mt.write_csv(f"./results/a_to_b_mt({self.iterations}).csv")
+        self.a_to_b_nmt.write_csv(f"./results/a_to_b_nmt({self.iterations}).csv")
+        self.b_to_a_nmt.write_csv(f"./results/b_to_a_nmt({self.iterations}).csv")
 
     def match_records(self):
         print("ODL DF")
@@ -73,7 +76,7 @@ class Reconciliator:
         print("JOIN DF")
         print(join_df)
         clean_join_df = self._process_duplicates(join_df, "_id")
-        self.a_to_b_mt = pl.concat([self.a_to_b_mt, clean_join_df])
+        self.a_to_b_mt = pl.concat([self.a_to_b_mt, clean_join_df], how="diagonal")
         print(self.a_to_b_mt)
 
     def not_match_records(self):
@@ -83,11 +86,21 @@ class Reconciliator:
             right_on=self.join_exp["b_columns"],
             how="anti",
         )
-        self.a_to_b_nmt = pl.concat([self.a_to_b_nmt, join_df])
+        self.a_to_b_nmt = pl.concat([self.a_to_b_nmt, join_df], how="diagonal")
         print(self.a_to_b_nmt)
+
+        join_df = self.file_df.join(
+            self.odl_df,
+            left_on=self.join_exp["b_columns"],
+            right_on=self.join_exp["a_columns"],
+            how="anti",
+        )
+        self.b_to_a_nmt = pl.concat([self.b_to_a_nmt, join_df], how="diagonal")
+        print(self.b_to_a_nmt)
 
     def new_conciliation_cycle(self):
         self.odl_df = self.a_to_b_nmt
         self.file_df = self.b_to_a_nmt
         self.a_to_b_nmt = pl.DataFrame()
         self.b_to_a_nmt = pl.DataFrame()
+        self.iterations += 1
